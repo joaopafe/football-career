5c089ec7df712554bcc55c678d613af021d60cd1
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2025-04-12 19:19:50 -0300

    add the StartFirstSeason component

---
+++ src/components/home/StartFirstSeason.tsx
@@ -0,0 +1,85 @@
+import { FunctionComponent, useState, useEffect } from "react";
+
+interface StartFirstSeason {
+  generatePlayer: () => void;
+}
+
+export const StartFirstSeason: FunctionComponent<StartFirstSeason> = ({
+  generatePlayer,
+}) => {
+  const [buttonIsDisabled, setButtonIsDisabled] = useState<boolean>(true);
+
+  useEffect(() => {
+    const nameInputElement = document.querySelector(
+      ".player-name-input"
+    ) as HTMLInputElement | null;
+    const initialOverallElement = document.querySelector(
+      ".player-name-input"
+    ) as HTMLElement | null;
+    const firstContractElement = document.querySelector(
+      ".first-contract"
+    ) as HTMLElement | null;
+
+    const getButtonIsDisabled = (): boolean => {
+      const inputsAreNull =
+        nameInputElement === null ||
+        initialOverallElement === null ||
+        firstContractElement === null;
+
+      if (!inputsAreNull) {
+        return (
+          nameInputElement.value === "" ||
+          initialOverallElement.innerText === "?" ||
+          firstContractElement.innerText === "?"
+        );
+      }
+
+      return true;
+    };
+
+    const overallObserver = new MutationObserver(() => {
+      setButtonIsDisabled(getButtonIsDisabled());
+    });
+
+    const inputListener = () => {
+      setButtonIsDisabled(getButtonIsDisabled());
+    };
+
+    if (nameInputElement) {
+      nameInputElement.addEventListener("input", inputListener);
+    }
+
+    if (initialOverallElement) {
+      overallObserver.observe(initialOverallElement, {
+        childList: true,
+        characterData: true,
+        subtree: true,
+      });
+    }
+
+    if (firstContractElement) {
+      overallObserver.observe(firstContractElement, {
+        childList: true,
+        characterData: true,
+        subtree: true,
+      });
+    }
+
+    return () => {
+      if (nameInputElement) {
+        nameInputElement.removeEventListener("input", inputListener);
+      }
+      overallObserver.disconnect();
+    };
+  }, []);
+
+  return (
+    <button
+      disabled={buttonIsDisabled}
+      onClick={generatePlayer}
+      className="start-first-season-button"
+    >
+      Iniciar a primeira temporada
+    </button>
+  );
+};

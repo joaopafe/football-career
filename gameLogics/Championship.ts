de242e230b8dd9ad2d2b3da4f73d63f66e8cd137
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2025-04-12 19:13:45 -0300

    add the Champioship class

---
+++ gameLogics/Championship.ts
@@ -0,0 +1,80 @@
+import { getRandomNumber } from "../utils/getRandomNumber";
+
+import {
+  ITeam,
+  ITeamsOverall,
+  ISeasonMatches,
+  ILeagueScore,
+} from "../interfaces/championshipInterfaces";
+
+export class Championship {
+  getTeamsOverall(teamsList: ITeam[]): ITeamsOverall[] {
+    const teamsOverall = teamsList.map((team) => {
+      return {
+        teamName: team.teamName,
+        teamOverallOnSeason: getRandomNumber(team.maxOverall, team.minOverall),
+      };
+    });
+
+    return teamsOverall;
+  }
+
+  getSeasonMatches(
+    teamsOverall: {
+      teamName: string;
+      teamOverallOnSeason: number;
+    }[],
+    playerTeamName: string,
+    playerTeamOverall: number
+  ): ISeasonMatches[] {
+    const opposingTeams = teamsOverall.filter((team) => {
+      return team.teamName != playerTeamName;
+    });
+
+    const seasonMatches = opposingTeams.map((team) => {
+      return {
+        opposingTeamName: team.teamName,
+        opposingTeamOverall: team.teamOverallOnSeason,
+        playerTeamName: playerTeamName,
+        playerTeamOverall: playerTeamOverall,
+      };
+    });
+
+    for (let i = 0; i < 19; i++) {
+      seasonMatches.push({
+        opposingTeamName: seasonMatches[i].opposingTeamName,
+        opposingTeamOverall: seasonMatches[i].opposingTeamOverall,
+        playerTeamName: seasonMatches[i].playerTeamName,
+        playerTeamOverall: seasonMatches[i].playerTeamOverall,
+      });
+    }
+
+    return seasonMatches;
+  }
+
+  getLeagueScore(
+    teamsList: ITeam[],
+    playerTeamName: string,
+    playerTeamScore: number
+  ): ILeagueScore[] {
+    const opposingTeams = teamsList.filter((team) => {
+      return team.teamName != playerTeamName;
+    });
+
+    const teamsScore = opposingTeams.map((team) => {
+      return {
+        teamName: team.teamName,
+        teamScore: getRandomNumber(team.maxOverall, team.minOverall),
+      };
+    });
+
+    teamsScore.push({
+      teamName: playerTeamName,
+      teamScore: playerTeamScore,
+    });
+
+    return teamsScore.sort((a, b) => {
+      return b.teamScore - a.teamScore;
+    });
+  }
+}

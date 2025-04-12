887bbaa5a6b547b8d5228f13e67cae297ee57b36
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2025-04-12 19:10:43 -0300

    add the national leagues informations

---
+++ data/nationalLeagues/nationalLeagues.ts
@@ -0,0 +1,19 @@
+import { brazilianChampionship } from "./brazilianTeams";
+import { englishChampionship } from "./englishTeams";
+import { frenchChampionship } from "./frenchTeams";
+import { germanChampionship } from "./germanTeams";
+import { italianChampionship } from "./italianTeams";
+import { portugueseChampionship } from "./portugueseTeams";
+import { spanishTChampionship } from "./spanishTeams";
+
+import { IChampionship } from "../../interfaces/championshipInterfaces";
+
+export const nationalLeagues: IChampionship[] = [
+  brazilianChampionship,
+  englishChampionship,
+  frenchChampionship,
+  germanChampionship,
+  italianChampionship,
+  portugueseChampionship,
+  spanishTChampionship,
+];

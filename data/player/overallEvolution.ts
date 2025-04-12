ee50638beb94098184def44c76837398e0ae55e8
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2025-04-12 19:11:44 -0300

    add the player data

---
+++ data/player/overallEvolution.ts
@@ -0,0 +1,122 @@
+export const overallEvolution = [
+  {
+    age: 17,
+    minimunOverall: 55,
+    maximunOverall: 65,
+  },
+  {
+    age: 18,
+    minimunOverall: 62,
+    maximunOverall: 75,
+  },
+  {
+    age: 19,
+    minimunOverall: 70,
+    maximunOverall: 78,
+  },
+  {
+    age: 20,
+    minimunOverall: 75,
+    maximunOverall: 82,
+  },
+  {
+    age: 21,
+    minimunOverall: 78,
+    maximunOverall: 85,
+  },
+  {
+    age: 22,
+    minimunOverall: 80,
+    maximunOverall: 88,
+  },
+  {
+    age: 23,
+    minimunOverall: 83,
+    maximunOverall: 89,
+  },
+  {
+    age: 24,
+    minimunOverall: 83,
+    maximunOverall: 89,
+  },
+  {
+    age: 25,
+    minimunOverall: 84,
+    maximunOverall: 90,
+  },
+  {
+    age: 26,
+    minimunOverall: 84,
+    maximunOverall: 90,
+  },
+  {
+    age: 27,
+    minimunOverall: 83,
+    maximunOverall: 90,
+  },
+  {
+    age: 28,
+    minimunOverall: 83,
+    maximunOverall: 90,
+  },
+  {
+    age: 29,
+    minimunOverall: 82,
+    maximunOverall: 88,
+  },
+  {
+    age: 30,
+    minimunOverall: 82,
+    maximunOverall: 87,
+  },
+  {
+    age: 31,
+    minimunOverall: 80,
+    maximunOverall: 86,
+  },
+  {
+    age: 32,
+    minimunOverall: 78,
+    maximunOverall: 84,
+  },
+  {
+    age: 33,
+    minimunOverall: 75,
+    maximunOverall: 83,
+  },
+  {
+    age: 34,
+    minimunOverall: 73,
+    maximunOverall: 78,
+  },
+  {
+    age: 35,
+    minimunOverall: 70,
+    maximunOverall: 75,
+  },
+  {
+    age: 36,
+    minimunOverall: 67,
+    maximunOverall: 72,
+  },
+  {
+    age: 37,
+    minimunOverall: 63,
+    maximunOverall: 68,
+  },
+  {
+    age: 38,
+    minimunOverall: 59,
+    maximunOverall: 64,
+  },
+  {
+    age: 39,
+    minimunOverall: 55,
+    maximunOverall: 62,
+  },
+  {
+    age: 40,
+    minimunOverall: 54,
+    maximunOverall: 61,
+  },
+];
